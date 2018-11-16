eb03634f0f4a29a5ba2ebf4e421eed88bfc5b66c
António Ramadas <antonio_ramadas@hotmail.com>
2018-11-16 21:33:46 +0000

    Implement authentication process

---
+++ authentication.js
@@ -0,0 +1,76 @@
+const SpotifyAuthentication = require('spotify-authentication');
+const Store = require('electron-store');
+const http = require('http');
+const { BrowserWindow } = require('electron');
+
+const spotifyAuthentication = new SpotifyAuthentication();
+let window;
+
+class Authentication {
+    constructor() {
+        this.store = new Store(/* {
+            defaults: {
+                accessToken: '',
+                refreshToken: '',
+            },
+        } */);
+
+        spotifyAuthentication.setHost('https://spotify-proxy-oauth2.herokuapp.com/');
+    }
+
+    newAuthentication() {
+        window = new BrowserWindow();
+        window.loadURL(spotifyAuthentication.createAuthorizeURL().href);
+
+        return new Promise((resolve, reject) => {
+            const server = http.createServer((req, res) => {
+                const code = req.url.match(/code=(.+)/)[1];
+
+                res.end();
+
+                if (code) {
+                    window.close();
+                    server.close();
+                    this.getRefreshToken(code)
+                        .then(resolve, reject);
+                }
+            }).listen(1995);
+        });
+    }
+
+    getRefreshToken(code) {
+        return spotifyAuthentication.authorizationCodeGrant(code)
+            .then((res) => {
+                this.store.set('refreshToken', res.refresh_token);
+                this.setAccessToken.bind(this)(res);
+            });
+    }
+
+    setAccessToken(res) {
+        this.store.set('accessToken', res.access_token);
+
+        // REVIEW: Should the access token be updated in the background constantly?
+        setTimeout(this.updateAccessToken.bind(this), res.expires_in - 100);
+    }
+
+    updateAccessToken() {
+        return spotifyAuthentication.refreshAccessToken(this.store.get('refreshToken'))
+            .then(this.setAccessToken.bind(this))
+            .catch(() => {
+                this.store.delete('refreshToken');
+                return this.newAuthentication();
+            });
+    }
+
+    authenticate() {
+        let f = this.newAuthentication.bind(this);
+
+        if (this.store.has('refreshToken')) {
+            f = this.updateAccessToken.bind(this);
+        }
+
+        return f();
+    }
+}
+
+module.exports = Authentication;
--- index.js
+++ index.js
@@ -1,76 +1,12 @@
 const { app, Menu, Tray } = require('electron');
 const path = require('path');
-const SpotifyWebApi = require('spotify-web-api-node');
-const http = require('http');
-const Store = require('electron-store');
-
-const store = new Store({
-    defaults: {
-        clientId: '',
-        clientSecret: '',
-        redirectUri: '',
-    },
-});
+const Authentication = require('./authentication');
 
 let tray;
-let callbackServer;
-let spotifyApi;
-
-// TODO add Heroku server
-// TODO add instructions to configure both Spotify and Heroku
-// TODO allow further customization (redirectUri, server instead of local)
-
-function createServer() {
-    callbackServer = http.createServer((req, res) => {
-        const code = req.url.match(/code=(.+)/)[1];
-
-        res.end();
-
-        spotifyApi.authorizationCodeGrant(code).then(
-            /* eslint-disable */
-            (data) => {
-                console.log(`The token expires in ${data.body.expires_in}`);
-                console.log(`The access token is ${data.body.access_token}`);
-                console.log(`The refresh token is ${data.body.refresh_token}`);
-
-                // Set the access token on the API object to use it in later calls
-                spotifyApi.setAccessToken(data.body.access_token);
-                spotifyApi.setRefreshToken(data.body.refresh_token);
-            },
-            (err) => {
-                console.log('Something went wrong!', err);
-            },
-            /* eslint-enable */
-        );
-        // TODO close browser window
-        // TODO activate spotify api, but first grab what we want (token)
-        // TODO close server
-    });
-
-    callbackServer.listen(1995); // TODO move this constant to a configuration file
-}
+let authentication;
 
 function spotify() {
-    const scopes = ['user-read-playback-state'];
-    const credentials = {
-        redirectUri: store.get('redirectUri'),
-        clientId: store.get('clientId'),
-        clientSecret: store.get('clientSecret'),
-    };
-
-    // Setting credentials can be done in the wrapper's constructor,
-    // or using the API object's setters.
-    spotifyApi = new SpotifyWebApi(credentials);
-
-    // Create the authorization URL
-    const authorizeURL = spotifyApi.createAuthorizeURL(scopes);
-
-    /* eslint-disable */
-    // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
-    console.log(authorizeURL);
-    /* eslint-enable */
-
-    createServer();
+    authentication.authenticate().then(console.log, console.error);
 }
 
 function createTray() {
@@ -82,17 +18,13 @@ function createTray() {
 
     tray.setContextMenu(contextMenu);
 
-    // TODO this should be a pop-up to the user
-    // In the meantime use environment variables
-    if (!store.get('clientId')) {
-        store.set({
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            redirectUri: process.env.REDIRECT_URI,
-        });
-    }
+    authentication = new Authentication();
 
     spotify();
 }
 
 app.on('ready', createTray);
+
+// To prevent the login window when closed to quit the application
+// https://github.com/electron/electron/blob/master/docs/api/app.md#event-window-all-closed
+app.on('window-all-closed', () => {});
